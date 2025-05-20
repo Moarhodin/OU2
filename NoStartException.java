8ad2c8f9bf4b78e1b20f32f75fd556639536884f
Moarhodin <159525977+Moarhodin@users.noreply.github.com>
2025-05-20 10:24:02 +0200

    Create NoStartException.java

---
+++ NoStartException.java
@@ -0,0 +1,8 @@
+				
+/**
+ * Exception thrown when the start node is not found in the maze.
+ * @author johane
+ * @version 2024-03-18
+ */
+public class NoStartException extends RuntimeException {
+}

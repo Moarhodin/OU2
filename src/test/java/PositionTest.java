d8216625c232d13d11110a8a0b77262843fa3f14
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 17:06:16 +0200

    Nya test för att testa om equals fungerar som de ska. Alla test OK

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -2,12 +2,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionTest{
+
+    /*Testar GetX och GetY*/
     @Test
-    void testGetXGetY(){
+     void testGetXGetY(){
         Position p= new Position(1,1);
         assertEquals(1, p.getX());
         assertEquals(1, p.getY());
     }
+
+    /*Testar Equals, ska reeturnera sant*/
+    @Test
+    void testEqualsTrue(){
+        Position p1= new Position(1,1);
+        Position p2= new Position(1,1);
+        assertEquals(p1, p2);
+    }
+
+    /*Testar Equals, ska reeturnera falskt*/
+    @Test
+    void testEqualsFalse(){
+        Position p1= new Position(1,1);
+        Position p2= new Position(2,1);
+        assertNotEquals(p1, p2);
+    }
+
+
 
 
 }

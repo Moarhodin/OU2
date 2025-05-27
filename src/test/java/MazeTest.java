4135beb706f2eb281a239785b23505684311ebc2
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 18:40:55 +0200

    Lagt till test för att se att isGoal inte retturnerar true för en position som inte är mål.

--- src/test/java/MazeTest.java
+++ src/test/java/MazeTest.java
@@ -23,11 +23,21 @@ public class MazeTest{
 
     /*Testar att isGoal hittar rätt mål*/
     @Test
-    void testIsGoal() throws IOException{
+    void testIsGoalTrue() throws IOException{
         Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
         Maze m=new Maze(s);
         Position goal = new Position(8,6);
         assertTrue(m.isGoal(goal));
+
+    }
+
+    /*Testar att isGoal inte returnerar mål för en position som inte är mål*/
+    @Test
+    void testIsGoalFalse() throws IOException{
+        Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
+        Maze m=new Maze(s);
+        Position p = new Position(1,1);
+        assertFalse(m.isGoal(p));
 
     }
 

28fcb63c727f89d195502292b394c5ba6b100740
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 20:52:19 +0200

    LeftHandRuleRobot testa att roboten följer vänsterhandsregeln när den inte har en vägg i nänrheten, dvs går rakt fram tills denn hittar vägg och följer sedan den med vänster hand.

--- src/test/java/LeftHandRuleRobotTest.java
+++ src/test/java/LeftHandRuleRobotTest.java
@@ -108,6 +108,34 @@ public class LeftHandRuleRobotTest{
         assertEquals(p5, robot.getPosition());
     }
 
+    /*Testa att roboten följer reglerna när den inte har en väggg i närheten
+    (går rakt fram tills den hittar en vägg)*/
+    @Test
+    void testRobotStartInTheMiddle() throws IOException{
+
+        Scanner s = new Scanner(new File("src/test/resources/maze4.txt"));
+        Maze m=new Maze(s);
+        LeftHandRuleRobot robot= new LeftHandRuleRobot(m);
+
+        Position p1= new Position(3,2);
+        assertEquals(p1, robot.getPosition());
+
+        robot.move();
+        Position p2= new Position(3,3);
+        assertEquals(p2, robot.getPosition());
+
+        robot.move();
+        Position p3= new Position(2,3);
+        assertEquals(p3, robot.getPosition());
+
+        robot.move();
+        Position p4= new Position(2,4);
+        assertEquals(p4, robot.getPosition());
+
+        assertTrue(robot.hasReachedGoal());
+    }
+
+
 
 
 

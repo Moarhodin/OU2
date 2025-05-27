c602f095e76669206e0745ed2f37c6caec2b3f4d
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 19:48:13 +0200

    Lagt till test för att testa att RandomRobot har gått i mål efter 1000 förflyttningar.

--- src/test/java/RandomRobotTest.java
+++ src/test/java/RandomRobotTest.java
@@ -9,7 +9,7 @@ public class RandomRobotTest{
 
     /*Kollar att roboten startar på start positionen*/
     @Test
-    void testGetPositionRobot() throws IOException{
+    void testGetPositionRandomRobot() throws IOException{
 
         Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
         Maze m=new Maze(s);
@@ -19,5 +19,22 @@ public class RandomRobotTest{
         assertEquals(p, robot.getPosition());
 
     }
+
+    /*Testar att roboten har gått i mål efter 1000 förflytttningar*/
+    @Test
+    void testHasReachedGoalRandomRobot() throws IOException{
+
+        Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
+        Maze m=new Maze(s);
+        RandomRobot robot= new RandomRobot(m);
+
+        while(!(robot.hasReachedGoal())){
+            robot.move();
+        }
+        assertTrue(robot.hasReachedGoal());
+
+    }
+
+
 
 }

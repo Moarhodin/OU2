b673c905e8fcc2b84f371a8505178b6cd19b0cf5
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 16:58:41 +0200

    Ny klass PositionTest, ett test för GetX och GetY. Test lyckades.

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PositionTest{
+    @Test
+    void testGetXGetY(){
+        Position p= new Position(1,1);
+        assertEquals(1, p.getX());
+        assertEquals(1, p.getY());
+    }
+}

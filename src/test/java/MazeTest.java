b53f6b91f72acc80965e364fc6c597724408dd5e
ens21mrn <ens21mrn@cs.umu.se>
2025-05-27 19:13:34 +0200

    Testar att isMovable returnerar true för lediga-, start- och målpositoner, samtf falskt för väggar.

--- src/test/java/MazeTest.java
+++ src/test/java/MazeTest.java
@@ -41,4 +41,31 @@ public class MazeTest{
 
     }
 
+    /*Testar att positioner som roboten ska kunna gå till returnerar true för isMovable*/
+    @Test
+    void testIsMovableTrue() throws IOException{
+        Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
+        Maze m=new Maze(s);
+        Position p = new Position(1,1);
+        Position start= new Position(1,0);
+        Position goal= new Position(8,6);
+        assertTrue(m.isMovable(p));
+        assertTrue(m.isMovable(start));
+        assertTrue(m.isMovable(goal));
+
+    }
+
+    /*Testar att positioner som roboten inte ska kunna gå till returnerar false för isMovable*/
+    @Test
+    void testIsMovableFalse() throws IOException{
+        Scanner s = new Scanner(new File("src/test/resources/maze.txt"));
+        Maze m=new Maze(s);
+        Position wall = new Position(0,0);
+        assertFalse(m.isMovable(wall));
+
+    }
+
+
+
+
 }

e42127254e11f4b07559bc7cac25e79d8a531e85
ens21mrn <ens21mrn@cs.umu.se>
2025-05-29 14:59:32 +0200

    Nya enhetliga kommentarer för alla klasser

--- src/main/java/org/example/RandomRobot.java
+++ src/main/java/org/example/RandomRobot.java
@@ -2,18 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A robot that moves in a random direction
+ * Description: A robot that moves in a random direction
  * @name RandomRobot
  * @author johane, ens21mrn
  * @version 2 2025-05-27
  */
+
 public class RandomRobot implements Robot{
     private Position pos;
     private Maze maze;
     private Position previousPosition;
 
     /**
-     * Construct a robot in a given maze.
+     * Description:Construct a robot in a given maze.
      * @param m the maze the robot should explore
      */
     public RandomRobot(Maze m) {
@@ -23,7 +24,7 @@ public class RandomRobot implements Robot{
     }
 
     /**
-     * Move this robot. Tries to move in a random direction.
+     * Description: Move this robot. Tries to move in a random direction.
      * Avoids going back from where it came if possible
      */
     @Override
@@ -38,7 +39,7 @@ public class RandomRobot implements Robot{
     }
 
     /**
-     * Get the neighbours of the current position that are movable.
+     * Description: Get the neighbours of the current position that are movable.
      * Excludes the position the robot came from
      * @return the available positions
      */
@@ -46,16 +47,18 @@ public class RandomRobot implements Robot{
         ArrayList<Position> movable = new ArrayList<>();
         List<Position> neighbours = getNeighbours();
         for(Position p:neighbours) {
-            if(maze.isMovable(p)&&!p.equals(previousPosition))
+
+            if(maze.isMovable(p)&&!p.equals(previousPosition)) {
                 movable.add(p);
+            }
+
         }
         return movable;
     }
 
     /**
-     * Move to a random position from the list of movable positions
+     * Description:  Move to a random position from the list of movable positions
      * @param movable the list of movable positions
-
      */
     private void moveToRandom(ArrayList<Position> movable) {
 
@@ -69,14 +72,14 @@ public class RandomRobot implements Robot{
     }
 
     /**
-     * Step back to the previous position
+     * Description: Step back to the previous position
      */
     private void stepBack() {
         setPosition(previousPosition);
     }
 
     /**
-     * Get the neighbours of the current position
+     * Description: Get the neighbours of the current position
      * @return a list of the neighbours
      */
     private ArrayList<Position> getNeighbours() {
@@ -89,7 +92,7 @@ public class RandomRobot implements Robot{
     }
 
     /**
-     * Get the current position of the robot
+     * Description: Get the current position of the robot
      * @return the position
      */
 
@@ -98,7 +101,7 @@ public class RandomRobot implements Robot{
     }
 
     /**
-     * Check if the robot has reached the goal
+     * Description: Check if the robot has reached the goal
      * @return true if the robot has reached the goal
      */
     public boolean hasReachedGoal() {
